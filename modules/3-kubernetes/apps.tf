850cee7b1092a1de2ee4f4f001122f3b2f01f46a
Andrii Aheiev <a.ageyev@gmail.com>
2024-07-01 21:49:06 +0200

    add petstore app

--- modules/3-kubernetes/apps.tf
+++ modules/3-kubernetes/apps.tf
@@ -1,19 +1,19 @@
+/*
 module "petstore" {
   depends_on = [module.istio]
   source     = "./modules/kubernetes-petstore"
   dns_name   = "petstore.${data.azurerm_dns_zone.dns_zone.name}"
 }
 
-/*
 module "bookinfo" {
   depends_on = [module.istio]
-  source     = "modules/kubernetes-bookinfo"
+  source     = "./modules/kubernetes-bookinfo"
   dns_name   = "bookinfo.${data.azurerm_dns_zone.dns_zone.name}"
 }
 
 module "podinfo" {
   depends_on = [module.istio]
-  source     = "../../modules/kubernetes-podinfo"
+  source     = "./modules/kubernetes-podinfo"
   dns_name   = "podinfo.${data.azurerm_dns_zone.dns_zone.name}"
 }
 */
